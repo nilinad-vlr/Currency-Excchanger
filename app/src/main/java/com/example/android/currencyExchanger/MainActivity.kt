523a69d43f7067c0ae844cb7e3937e51c0aef53a
Danilin2003 <nilinad.vlr@gmail.com>
2020-06-07 16:03:19 +0300

    Верстка + добавления трекинга

---
+++ app/src/main/java/com/example/android/currencyExchanger/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.android.currencyExchanger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById<EditText>(R.id.editTextValue)
+
+        editText.afterTextChanged {
+
+            println(it.toInt())
+
+            val textView1: TextView = findViewById<TextView>(R.id.rusValue)
+            textView1.text = it.toFloat().toString()
+
+            val textView2: TextView = findViewById<TextView>(R.id.usaValue)
+            textView2.text = (it.toFloat() / 74).toString()
+        }
+
+    }
+
+    fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+                afterTextChanged.invoke(editable.toString())
+            }
+        })
+    }
+}
