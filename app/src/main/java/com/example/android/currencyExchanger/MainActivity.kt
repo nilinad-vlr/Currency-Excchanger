a73d30740279a8f613eb180d85190495b754e7ac
Danilin2003 <nilinad.vlr@gmail.com>
2020-06-08 18:15:39 +0300

    Изменение дизайна и добавление ввода с клавиатуры

--- app/src/main/java/com/example/android/currencyExchanger/MainActivity.kt
+++ app/src/main/java/com/example/android/currencyExchanger/MainActivity.kt
@@ -4,42 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inputText: TextView = findViewById<TextView>(R.id.inputText)
+        val outputText: TextView = findViewById<TextView>(R.id.outputText)
 
-        val editText: EditText = findViewById<EditText>(R.id.editTextValue)
-        val textView1: TextView = findViewById<TextView>(R.id.rusValue)
-        val textView2: TextView = findViewById<TextView>(R.id.usaValue)
-
-        editText.afterTextChanged {
-            if (it.isNotEmpty()) {
-                textView1.text = it.toFloat().toString()
-                textView2.text = (it.toFloat() / 74).toString()
+        btn0.setOnClickListener { setTextFields("0") }
+        btn1.setOnClickListener { setTextFields("1") }
+        btn2.setOnClickListener { setTextFields("2") }
+        btn3.setOnClickListener { setTextFields("3") }
+        btn4.setOnClickListener { setTextFields("4") }
+        btn5.setOnClickListener { setTextFields("5") }
+        btn6.setOnClickListener { setTextFields("6") }
+        btn7.setOnClickListener { setTextFields("7") }
+        btn8.setOnClickListener { setTextFields("8") }
+        btn9.setOnClickListener { setTextFields("9") }
+        btnAC.setOnClickListener {
+            inputText.text = ""
+            outputText.text = ""
+        }
+        btnBackSpace.setOnClickListener {
+            val str = inputText.text.toString()
+            if (str.isNotEmpty()) {
+                inputText.text = str.substring(0, str.length - 1)
             } else {
-                textView1.text = "0"
-                textView2.text = "0"
+                inputText.text = "0"
             }
+            outputText.text = ""
         }
     }
 
-    fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-        this.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun afterTextChanged(editable: Editable?) {
-                afterTextChanged.invoke(editable.toString())
-            }
-        })
+    fun setTextFields(str: String) {
+        inputText.text = inputText.text.toString().plus(str)
     }
 }
